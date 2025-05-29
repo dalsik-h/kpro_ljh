3b3f6bcefe860db3d51992e423eee84a1638faa9
dalsik-h <sjuho@kwater.or.kr>
2025-05-29 17:50:28 +0900

    Add files via upload

--- app.py
+++ app.py
@@ -242,6 +242,30 @@ if 'df' in st.session_state and 'kmeans' in st.session_state:
             fontsize=5, color='black', verticalalignment='top'
         )
 
+        # 포항시(분) 유량
+        col_c = "phb_flow_3"
+        col_c_title = "포항시(분) 유량"
+        base_x3, base_y3 = 1450, 130
+        ax.add_patch(patches.Rectangle(
+            (base_x3, base_y3 - 15), max_width, box_height,
+            linewidth=1, edgecolor='red', facecolor='lightpink', alpha=0.9
+        ))
+        ax.text(
+            base_x3 + 5, base_y3, col_c_title,
+            fontsize=5, weight='bold', color='black', verticalalignment='top', fontproperties=font_prop
+    
+        )
+        y3 = base_y3 + 1 * line_height
+        text = f"{rep_row[col_c]}"
+        ax.add_patch(patches.Rectangle(
+            (base_x3, y3 - 15), max_width, box_height,
+            linewidth=1, edgecolor='black', facecolor='white', alpha=0.9
+        ))
+        ax.text(
+            base_x3 + 5, y3, text,
+            fontsize=5, color='black', verticalalignment='top'
+        )
+
         # for i, (label, value) in enumerate(stats.items()):
         #     y = base_y + (i + 1) * line_height
         #     text = f"{label}: {value}"
