cec2cfb0d3b41b13b84c5999e7325b068c4f5f27
dalsik-h <sjuho@kwater.or.kr>
2025-09-04 14:31:54 +0900

    Add files via upload

--- main_app.py
+++ main_app.py
@@ -9,10 +9,10 @@ st.set_page_config(layout="wide")
 # 좌측 사이드바 버튼
 st.sidebar.markdown("## ✅ 포항권 공업용수<br>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;AI분석시스템", unsafe_allow_html=True)
 
-if st.sidebar.button("📈 영천댐 수위 예측  &nbsp;&nbsp;&nbsp;→&nbsp;&nbsp;&nbsp;&nbsp;  안계소수력 발전전력 예측"):
+if st.sidebar.button("📈 영천댐 수위 예측  &nbsp;&nbsp;&nbsp;→&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;  안계소수력 발전전력 예측"):
     st.session_state.selected  = "app_lstm.py"
 
-if st.sidebar.button("📊 공업용수 관망정보 &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;클러스터링"):
+if st.sidebar.button("📊 공업용수 관망정보 &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;클러스터링"):
     st.session_state.selected  = "app_gmm.py"
 
 selected = st.session_state.selected
